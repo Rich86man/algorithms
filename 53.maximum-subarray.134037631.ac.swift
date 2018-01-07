0c764840dbb667f54ed5c31368d3f39053443ce2
Richard Kirk <RichardBKirk@gmail.com>
2018-01-07 12:57:05 -0800

    added a DP Solution to 53

--- 53.maximum-subarray.134037631.ac.swift
+++ 53.maximum-subarray.134037631.ac.swift
@@ -26,21 +26,45 @@
  * the divide and conquer approach, which is more subtle.
  * 
  */
-class Solution {
+
+class DPSolution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        
+        guard nums.count > 1 else { return nums.first ?? 0 }
+
+        var dp: [Int] = [Int](repeating: 0, count: nums.count)
+        dp[0] = nums[0]
+
+        for index in 1..<nums.count {
+            // take the last value, or start over
+            dp[index] = max(dp[index - 1] + nums[index], nums[index])
+        }
+        return dp.sorted().last!
+    }
+}
+
+class O1StorageSolution {
+
+    func maxSubArray(_ nums: [Int]) -> Int {
+
         var maxSum = nums[0]
         var runningSum = nums[0]
-        
+
         for index in 1..<nums.count {
-            if runningSum < 0 { 
+            if runningSum < 0 {
                 runningSum = nums[index]
             } else {
                 runningSum += nums[index]
             }
             maxSum = max(maxSum, runningSum)
         }
-        
+
         return maxSum
+    }
+}
+
+
+class Solution {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        return O1StorageSolution().maxSubArray(nums)
     }
 }
